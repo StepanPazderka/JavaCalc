ae59d275fe4f8cddfbc1132b9a06101f64d4eb0a
Stepan Pazderka <stepan.pazderka@icloud.com>
2020-06-27 20:17:29 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static float multiply(float a, float b)
+    {
+        return a*b;
+    }
+
+    public static float divide(float a, float b)
+    {
+        return a/b;
+    }
+
+    public static float addition(float a, float b)
+    {
+        return a+b;
+    }
+
+    public static float subtraction(float a, float b)
+    {
+        return a-b;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scanner;
+        float firstNum, secondNum, result;
+        String operation;
+
+        scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("Select operation (*,/,+,-)");
+            operation = scanner.next();
+            if (operation.matches("[*/+-]")) {
+                break;
+            } else {
+                System.out.println("Invalid operation");
+            }
+        }
+        System.out.println("Type in first number");
+        firstNum = scanner.nextFloat();
+
+        System.out.println("Type in second number");
+        secondNum = scanner.nextFloat();
+
+        switch(operation)
+        {
+            case "*": result = multiply(firstNum, secondNum); break;
+            case "/": result = divide(firstNum, secondNum); break;
+            case "+": result = addition(firstNum, secondNum); break;
+            case "-": result = subtraction(firstNum, secondNum); break;
+            default: result = 0;
+        }
+
+        System.out.println("Result of your operation is " + result);
+    }
+}
